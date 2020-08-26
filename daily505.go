6d6518f36773759bb6967bac526544c5d64f02d8
uffe hellum <uffe.hellum@gmail.com>
2020-08-25 20:40:50 -0700

    505

---
+++ daily505.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{}
+	for i := 1; i <= 50; i++ {
+		a = append(a, i)
+	}
+	a = rotate2(a, 49)
+	fmt.Println(a)
+}
+
+func rotate2(a []int, r int) []int {
+	i := len(a) - r
+	return append(a[i:], a[:i]...)
+}
+
+func rotate(a []int, r int) {
+	n := len(a)
+	p0 := -1
+	p := p0
+	t := 0
+	for i := n; i > 0; i-- {
+		if p == p0 {
+			p0++
+			p = p0
+			t = a[p]
+		}
+		p = (p + r) % n
+		t, a[p] = a[p], t
+	}
+}
