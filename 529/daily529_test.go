d7567ea2fb89a9eb9dfb17326b54150b2ba02bf9
uffe hellum <uffe.hellum@gmail.com>
2020-09-18 13:23:07 -0700

    529

---
+++ 529/daily529_test.go
@@ -0,0 +1,39 @@
+package daily529
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test529(t *testing.T) {
+	assertClip(t, "bob", []string{"bob"})
+	assertClip(t, "racecarannakayak",
+		[]string{"racecar", "anna", "kayak"})
+}
+
+func assertClip(t *testing.T, source string, expected []string) {
+	actual := ClipPalindromes(source)
+	if fmt.Sprint(actual) != fmt.Sprint(expected) {
+		t.Fatal(fmt.Sprintf(
+			"SplitPalindromes(source=%s) = %v, expected %v",
+			source, actual, expected))
+	}
+}
+
+func ClipPalindromes(s string) []string {
+	r := []string{}
+	for s > "" {
+		l:
+		for n := len(s); n >= 1; n-- {
+			for i := (n - 1) / 2; i >= 0; i-- {
+				if s[i] != s[n-i-1] {
+					continue l
+				}
+			}
+			r = append(r, s[:n])
+			s = s[n:]
+			break
+		}
+	}
+	return r
+}
